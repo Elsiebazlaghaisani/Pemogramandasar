8e2fec7430e5fade3f78189dc58489d4b3e0a0a9
Elsiebazlaghaisani <111962029+Elsiebazlaghaisani@users.noreply.github.com>
2022-09-06 12:15:14 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Nama:Elsie Bazla Ghaisani"<< endl;
+    cout << "Kelas:X PPLG 1"<< endl;
+    cout << "========" << endl;
+    cout << "Angka1=10"<< endl;
+    cout << "Angka2=5" << endl;
+    cout << "Hasil :" << 10 + 5 << endl;
+    cout << "Hasil :" << 10 - 5 << endl;
+    return 0;
+    }
